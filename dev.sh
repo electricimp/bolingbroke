ce3b1376751fb7fa843ff4f558f5d3fd0e790775
Roger Lipscombe <roger@differentpla.net>
2015-04-30 13:43:46 +0100

    Do start_examples, also reduce interval to original in dev.

--- dev.sh
+++ dev.sh
@@ -2,4 +2,6 @@
 
 ./rebar get-deps
 ./rebar compile
-ERL_LIBS=$(pwd) erl -pa ebin -pa deps/*/ebin -s bolingbroke -bolingbroke port 18360 -sname bolingbroke
+ERL_LIBS=$(pwd) erl -sname bolingbroke \
+    -pa ebin -pa deps/*/ebin \
+    -s bolingbroke -bolingbroke port 18360 -bolingbroke start_examples true -bolingbroke update_interval_ms 1000
